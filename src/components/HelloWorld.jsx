543ccfd9b13759f2c75d082e1478feb05b73b0f2
Juan Pablo Leyva <jpleyvat@gmail.com>
2020-02-04 14:44:30 -0500

    Development environment created.

--- src/components/HelloWorld.jsx
+++ src/components/HelloWorld.jsx
@@ -1,7 +1,6 @@
-import React from "react";
+import React from 'react';
+import '../assets/styles/App.scss';
 
-const HelloWorld = () => {
-  <h1>Hello world</h1>;
-};
+const HelloWorld = () => <h1>Hello world</h1>;
 
 export default HelloWorld;
--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
-import React from "react";
-import ReactDOM from "react-dom";
+import React from 'react';
+import ReactDOM from 'react-dom';
 
-import HelloWorld from "./components/HelloWorld";
+import HelloWorld from './components/HelloWorld';
 
 ReactDOM.render(
   <HelloWorld />,
-  document.getElementById(app)
+  document.getElementById('app')
 );
