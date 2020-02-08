01a9052c18b17c5589792df01fa182efc04e1cf5
Juan Pablo Leyva <jpleyvat@gmail.com>
2020-02-08 10:23:56 -0500

    Last commit.

---
+++ Postman/app/resources/app/js/brace-text.js
@@ -0,0 +1,11 @@
+webpackJsonp([13],{
+
+/***/ 3569:
+/***/ (function(module, exports) {
+
+
+
+
+/***/ })
+
+});
---
+++ src/containers/App.jsx
@@ -0,0 +1,49 @@
+import React from 'react';
+
+import Header from '../components/Header';
+import Search from '../components/Search';
+import Categories from '../components/Categories';
+import Carousel from '../components/Carousel';
+import CarouselItem from '../components/CarouselItem';
+import Footer from '../components/Footer';
+
+import useInitialState from '../hooks/useInitialState';
+
+import '../assets/styles/App.scss';
+
+const API = 'http://localhost:3000/initalState';
+
+const App = () => {
+  const initialState = useInitialState(API);
+
+  return (
+    <div className='App'>
+      <Header />
+      <Search />
+      {initialState.mylist.length > 0 && (
+        <Categories title='My lists'>
+          <Carousel>
+            <CarouselItem />
+          </Carousel>
+        </Categories>
+      )}
+      <Categories title='Trends'>
+        <Carousel>
+          {initialState.trends.map(item => (
+            <CarouselItem key={item.id} {...item} />
+          ))}
+        </Carousel>
+      </Categories>
+      <Categories title='Originals PlatziVideo'>
+        <Carousel>
+          {initialState.originals.map(item => (
+            <CarouselItem key={item.id} {...item} />
+          ))}
+        </Carousel>
+      </Categories>
+      <Footer />
+    </div>
+  );
+};
+
+export default App;
