a3cf54e05a038cbfca96e242a489da6299d7d08a
Juan Pablo Leyva <jpleyvat@gmail.com>
2020-11-11 17:38:27 -0500

    Ads first route: Home

--- src/containers/Home.jsx
+++ src/containers/Home.jsx
@@ -13,7 +13,7 @@ import '../assets/styles/App.scss';
 
 const API = 'http://localhost:3000/initalState';
 
-const App = () => {
+const Home = () => {
   const initialState = useInitialState(API);
 
   return initialState.length === 0 ? <h1>Loading...</h1> : (
@@ -46,4 +46,4 @@ const App = () => {
   );
 };
 
-export default App;
+export default Home;
